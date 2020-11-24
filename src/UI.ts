7d4efb2ea3041a3668d9282876be1f15139b86ce
sr33 <sree.patlola@gmail.com>
2020-11-24 11:04:50 -0500

    mismatch type

--- src/UI.ts
+++ src/UI.ts
@@ -1,22 +1,22 @@
-import axios, {AxiosError} from 'axios';
+import axios, { AxiosError } from 'axios';
 import * as Vue from './static_resources/vue';
 import Profile from './Profile';
 
 export default class UI {
     readonly templateAbsoluteUrl = chrome.runtime.getURL('/static_resources/template.html');
     readonly body = document.getElementsByTagName('body')[0];
-    public vueApp: Vue;
+    public vueApp: any;
     public profile: Profile = new Profile();
 
     public async injectUI() {
         const html = await axios
-        .get(this.templateAbsoluteUrl)
-        .then((r) => {
-            return r.data;
-        })
-        .catch((error: AxiosError) => {
-            console.error(`error while trying to inject ui -> ${error}`);
-        });
+            .get(this.templateAbsoluteUrl)
+            .then((r) => {
+                return r.data;
+            })
+            .catch((error: AxiosError) => {
+                console.error(`error while trying to inject ui -> ${error}`);
+            });
         this.body.className = ''; // remove existing styles
         this.body.innerHTML = html;
         this.mountVueApp();
